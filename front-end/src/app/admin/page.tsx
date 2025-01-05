4d50e0ea7ff7142006ace469e2672de28f912ae6
Chocobo <moras.thomas@gmail.com>
2025-01-05 16:22:48 +0100

    fix import

--- front-end/src/app/admin/page.tsx
+++ front-end/src/app/admin/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import UsersTest from "@/components/admin/UsersTest";
+import { UsersTest } from "@/components/admin/UsersTest";
 import React from "react";
 
 const page = () => {
--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
-
 import Dashboard from "@/components/shared/Dashboard";
 import NotConnected from "@/components/shared/NotConnected";
+
 import { useAccount } from "wagmi";
 
 export default function Home() {
