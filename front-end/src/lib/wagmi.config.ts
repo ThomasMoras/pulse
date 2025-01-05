1bb0593e3b6420562a49cbdcee52182fa5cdc664
Chocobo <moras.thomas@gmail.com>
2025-01-05 17:41:17 +0100

    wagmi config

--- front-end/src/lib/wagmi.config.ts
+++ front-end/src/lib/wagmi.config.ts
@@ -3,10 +3,10 @@ import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { hardhat, sepolia } from "wagmi/chains";
 import { http } from "wagmi";
 
-console.log("WALLET_CONNECT_PROJECT_ID :", process.env.WALLET_CONNECT_PROJECT_ID);
+console.log("WALLET_CONNECT_PROJECT_ID :", process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID);
 export const config = getDefaultConfig({
   appName: "Pulse",
-  projectId: process.env.WALLET_CONNECT_PROJECT_ID || "",
+  projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || "",
   chains: [hardhat, sepolia],
   ssr: true,
   transports: {
