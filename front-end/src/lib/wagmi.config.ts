aeeacfb4b2bc540d994d992bf31b267d4f661835
Chocobo <moras.thomas@gmail.com>
2025-01-05 19:30:54 +0100

    adding base sepolia wagmi

--- front-end/src/lib/wagmi.config.ts
+++ front-end/src/lib/wagmi.config.ts
@@ -1,15 +1,16 @@
 import "@rainbow-me/rainbowkit/styles.css";
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { hardhat, sepolia } from "wagmi/chains";
+import { hardhat, sepolia, baseSepolia } from "wagmi/chains";
 import { http } from "wagmi";
 
 export const config = getDefaultConfig({
   appName: "Pulse",
   projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || "",
-  chains: [hardhat, sepolia],
+  chains: [hardhat, baseSepolia, sepolia],
   ssr: true,
   transports: {
     [hardhat.id]: http(),
-    [sepolia.id]: http(`${process.env.ALCHEMY_API_KEY}`),
+    [sepolia.id]: http(`${process.env.ETHER_SEPOLIA_RPC}`),
+    [baseSepolia.id]: http(`${process.env.BASE_SEPOLIA_RPC}`),
   },
 });
