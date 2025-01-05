9344359aec8280d3d53b729e6b5a33ee1e733c13
Chocobo <moras.thomas@gmail.com>
2025-01-05 15:40:53 +0100

    config next js

---
+++ front-end/next.config.js
@@ -0,0 +1,16 @@
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals.push("pino-pretty", "encoding");
+    return config;
+  },
+};
+
+module.exports = nextConfig;
